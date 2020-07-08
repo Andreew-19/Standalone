fa246642ee446762b179f6a82c4730b668d5485c
Mihnea Banditul <47335726+MihneaBanditul@users.noreply.github.com>
2020-07-09 02:16:15 +0300

    Update client.lua

--- HP-and-Armor-Diplsay/client.lua
+++ HP-and-Armor-Diplsay/client.lua
@@ -1,14 +1,14 @@
 Citizen.CreateThread(function()
     while true do Wait(1)
-        NiPsEeNvaletulMeu = GetPlayerPed(-1)
-        viata = (GetEntityHealth(NiPsEeNvaletulMeu)-100)
+    PlayerPed = GetPlayerPed(-1)
+    viata = (GetEntityHealth(PlayerPed)-100)
 	if viata >= 35 then 
     	text(0.035, 0.96, 0.35, 4, "~g~VIATA: ~w~".. viata .." ~g~HP", 255, 255, 255, true)
 	else
     	text(0.035, 0.96, 0.35, 4, "~r~VIATA: ~w~".. viata .." ~r~HP", 255, 255, 255, true)
-        end
-
-    armura = (GetPedArmour(NiPsEeNvaletulMeu))
+    end
+    
+    armura = (GetPedArmour(PlayerPed))
 
     if armura > 1 then
         text(0.110, 0.96, 0.35, 4, "~b~ARMURA: ~w~".. armura .." ~b~%", 255, 255, 255, true)
